65c81294a1704274ae882c7b32e317d2a19572d1
mig-ramos <mig.ramos10@gmail.com>
2024-12-07 12:43:28 -0300

    feat: all methods, id for number use pipe transformer

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
-// import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
+// import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
 import { CreateUserDTO } from 'src/dto/create-user.dto';
 import { UpdatePatchUserDTO } from 'src/dto/update-patch-user.dto';
 import { UpdatePutUserDTO } from 'src/dto/update-put-user.dto';
@@ -17,37 +17,60 @@ export class UserController {
         return { users: [] }
     }
 
+    // @Get(':id')
+    // async show(@Param() param) {
+    //     return { user: {}, param }
+    // }
+
     @Get(':id')
-    async show(@Param() param) {
-        return { user: {}, param }
+    async show(@Param('id', ParseIntPipe) id: number) {
+        return { user: {}, id }
     }
 
+    // @Put(':id')
+    // async update(@Body() { name, email, password }: UpdatePutUserDTO, @Param() param) {
+    //     return {
+    //         method: 'put',
+    //         name, email, password,
+    //         param
+    //     }
+    // }
+
     @Put(':id')
-    async update(@Body() { name, email, password }: UpdatePutUserDTO, @Param() param) {
+    async update(@Body() { name, email, password }: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number) {
         return {
             method: 'put',
             name, email, password,
-            param
+            id
         }
     }
 
+    // @Patch(':id')
+    // async updatePartial(@Body() { name, email, password }: UpdatePatchUserDTO, @Param() param) {
+    //     return {
+    //         method: 'patch',
+    //         name, email, password,
+    //         param
+    //     }
+    // }
+
     @Patch(':id')
-    async updatePartial(@Body() { name, email, password }: UpdatePatchUserDTO, @Param() param) {
+    async updatePartial(@Body() { name, email, password }: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number) {
         return {
             method: 'patch',
             name, email, password,
-            param
+            id
         }
     }
 
-    @Delete(':id')
-    async delete(@Param('id') id) {
-        return { id }
-    }
-
-    // No caso do Id ser número, esta alteraçõ deve ser feita em toda a api
     // @Delete(':id')
-    // async delete(@Param('id', ParseIntPipe) id: number) {
+    // async delete(@Param('id') id) {
     //     return { id }
     // }
+
+    // No caso do Id ser número, esta alteraçõ deve ser feita em toda a api
+    @Delete(':id')
+    async delete(@Param('id', ParseIntPipe) id: number) {
+        return { id }
+    }
 }
