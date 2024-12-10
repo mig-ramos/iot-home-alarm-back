f2577c8a9a7d3202aa710b69fe54d96546d28293
mig-ramos <mig.ramos10@gmail.com>
2024-12-09 22:05:25 -0300

    feat: install and configure cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['*']
+  });
+
   app.useGlobalPipes(new ValidationPipe()); // Monitorando as rotas
 
   app.useGlobalInterceptors(new LogInterceptor)
